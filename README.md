048fcc4232b5076de632934a1a06b64af679227a
Francisco Gómez <pacojagova@gmail.com>
2024-11-11 13:50:19 -0600

    Parte 2 - Gestor de tareas

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ En este ejercicio tiene como objetivo repasar los siguientes tema:
 ## Contenidos
 
 1. [Parte 1: Navbar Responsiva](https://drive.google.com/file/d/18FFo7OQOhhcaegsbYL7xGOZUWNC828Lk/view?usp=drive_link)
+1. [Parte 2: Gestor de tareas](https://uaslpedu.sharepoint.com/sites/Test494/_layouts/15/stream.aspx?id=%2Fsites%2FTest494%2FShared%20Documents%2FGeneral%2FRecordings%2FMicrosoft%20Teams%20meeting%2D20241111%5F132900%2DMeeting%20Recording%2Emp4&referrer=StreamWebApp%2EWeb&referrerScenario=AddressBarCopied%2Eview%2E01efa9b1%2D7278%2D404a%2Da0f2%2D25cf0216ac17)
--- src/script.js
+++ src/script.js
@@ -7,3 +7,29 @@ function showMenu() {
         mobileMenu.classList.add(className);
     }
 }
+
+function addTask(){
+    const container = document.querySelector(".main-container");
+    const taskInput = document.getElementById("taskInput");
+
+    if(taskInput.value.trim()){
+        const card = document.createElement("div");
+        card.classList.add("card");
+        
+        const text = document.createElement("p");
+        text.textContent = taskInput.value;
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Eliminar";
+
+        deleteBtn.onclick = function () {
+            container.removeChild(card);
+        }
+
+        card.appendChild(text);
+        card.appendChild(deleteBtn);
+        container.appendChild(card);
+
+        taskInput.value = "";
+    }
+}
